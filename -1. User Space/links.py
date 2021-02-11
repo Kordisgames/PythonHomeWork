016a30194c8c0dda2c8459cf5b4fc059e29fbf22
Vyacheslav Boo <nesterenko.v@donnu.edu.ua>
2021-02-11 20:23:26 +0000

    Sorted and All Basic Module

--- -1. User Space/links.py
+++ -1. User Space/links.py
@@ -1,15 +1,13 @@
 # Сюда кидаем ссылки на ваши репозитории по модулям
 # Я сделаю 1 репозиторий, но в нём будут просто разные папки, в этих папках я и буду хранить все задания. Только если это не проекты.
 '''
-Модуль 1 - 31.10.2020.
 1. Анастасия Соловьёва - https://repl.it/@Anastasiya0111/modle1
 2. Анстасия Радзиховская - https://repl.it/@AnastasiaRad/AcrobaticPreemptiveAnalysts
 3. Виталий Насиковский - https://repl.it/@ncxofficial/FTP102020#README.txt
 4. Сімон Катя - https://repl.it/@MrsHatake/PhytonHomeWorkAllTasks
 5. Пешехонова Алекса - https://repl.it/@Aleksa200/PythonHomeWorkAllTasks
-6. Нестеренко Вячеслав - https://repl.it/@nesterenkov/PyhonHomeWorkAllTasks
-
-Модуль 2 - 12.11.2020.
-Модуль 3 - 12.11.2020.
-
+6. Нестеренко Вячеслав - https://repl.it/@nesterenkov/PhytonHomeWorkAllTasks
+7. Оврамець Ілля - 
+https://repl.it/@litlprogram/Module-Home-vork-1#Modul/modul3
+8. Явгусішин Богдан - https://repl.it/@Scrag0/PythonHomeWorkEx#ch_4.py
 '''
